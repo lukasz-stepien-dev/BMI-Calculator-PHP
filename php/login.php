dea90b140f0354ed64345fa34a1f630b60d83086
Łukasz Stępień <lukaszatepien@gmail.com>
2023-09-11 21:33:00 +0200

    Get user from database

--- php/login.php
+++ php/login.php
@@ -1,1 +1,15 @@
 <?php
+session_start();
+$pdo = require_once "database.php";
+
+if (!empty($_POST["name"]) || !empty($_POST["surname"]) || !empty($_POST["password"])) {
+    $name = htmlentities($_POST["name"], ENT_QUOTES, "UTF-8");
+    $surname = htmlentities($_POST["surname"], ENT_QUOTES, "UTF-8");
+    $password = htmlentities($_POST["password"], ENT_QUOTES, "UTF-8");
+
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $user = $pdo->query("SELECT * FROM users 
+         WHERE name = '$name' AND surname = '$surname' AND password = '$hashed_password'");
+    $user->fetch(PDO::FETCH_ASSOC);
+    echo var_dump($user);
+}
