571cd78e3bcc8bb36fb4abb74ebd7d84a6c11b21
Łukasz Stępień <lukaszatepien@gmail.com>
2023-09-11 17:52:31 +0200

    Added html to creating new account

--- php/login.php
+++ php/login.php
@@ -7,14 +7,15 @@ if ($conn->connect_errno != 0) {
 } else {
     $name = $_POST['name'];
     $surname = $_POST['surname'];
-    $password = $_POST['password'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-    $stmt = "SELECT * FROM users WHERE name = '$name' AND surname = '$surname' AND password = '$password'";
+    $stmt = "SELECT * FROM Users WHERE name = '$name' AND surname = '$surname' AND password = '$password'";
 
     if ($result = @$conn->query($stmt)) {
         $num_users = $result->num_rows;
+        echo $num_users;
         if ($num_users = 0) {
-            
+            header("Location: ../create-account.html");
         }
     }
     $conn->close();
